fbc2ae0a7b4193e0c19a50cb05c822cc4cb6cf90
savoca <adeddo27@gmail.com>
2014-07-29 13:28:27 +0000

    Add i3lock with nice script :)

---
+++ bin/lock.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+cmustat=$(echo "$(cmus-remote -Q 2>/dev/null)" | grep -o 'status [^\$]\+' | cut -d " " -f 2- )
+if [ "$cmustat" = "playing" ]; then
+	cmus-remote -u
+fi
+i3lock -i /tmp/screen.png
