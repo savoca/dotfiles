12d1af604cd1b35ee119a096b43bf08ee2c28635
savoca <adeddo27@gmail.com>
2015-03-30 23:54:05 +0000

    Update .bin binaries and scripts
     - Also add an alias to check linux mainline

--- .bin/android-rules.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget http://savoca.codefi.re/priv/shared/resources/51-android.rules
-sudo mv 51-android.rules /etc/udev/rules.d/51-android.rules
-sudo chmod 0644 /etc/udev/rules.d/51-android.rules
-sudo udevadm trigger
