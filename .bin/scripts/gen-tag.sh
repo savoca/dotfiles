a9ee629419b5a9d8e1cd27c47b4914eadd951b95
savoca <adeddo27@gmail.com>
2015-04-10 16:00:15 +0000

    gen-tag.sh: Capitalize the entire tag

--- .bin/scripts/gen-tag.sh
+++ .bin/scripts/gen-tag.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PROJECT=$(basename $(pwd))
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-BASEPRJ=${PROJECT^^}
-BASEBRCH=${BRANCH^^}
 TAG=$(printf "%x" $(git rev-list HEAD --count))
-echo ${BASEPRJ:0:2}${BASEBRCH:0:2}$TAG
+FULLTAG=${PROJECT:0:2}${BRANCH:0:2}$TAG
+echo ${FULLTAG^^}
