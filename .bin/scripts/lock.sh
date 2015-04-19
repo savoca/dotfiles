fcd3d6b8549915da74765eda6e1ca455cab4190e
savoca <adeddo27@gmail.com>
2015-04-19 13:12:58 -0400

    lock.sh: Add custom lock screen icon
     - Unlocked: http://i.imgur.com/VppqBRj.jpg
     - Locked: http://i.imgur.com/W34yb2J.png

--- .bin/scripts/lock.sh
+++ .bin/scripts/lock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+[[ -f $HOME/Pictures/Icons/lock.png ]] && convert /tmp/screen.png $HOME/Pictures/Icons/lock.png -gravity center -composite -matte /tmp/screen.png
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 i3lock -u -i /tmp/screen.png
