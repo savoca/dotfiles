14bc4fbd5e651465f388900b261bf336adb33972
savoca <adeddo27@gmail.com>
2016-03-07 15:01:40 -0800

    bootconfig: Set default brightness on bootup

--- extra/usr/bin/bootconfig.sh
+++ extra/usr/bin/bootconfig.sh
@@ -11,3 +11,4 @@ chmod 0666 /sys/class/leds/smc\:\:kbd_backlight/brightness
 ## 2015 rMBP (and maybe others) gmux_backlight control
 setpci -v -H1 -s 00:01.00 BRIDGE_CONTROL=0
 chmod 0666 /sys/class/backlight/gmux_backlight/brightness
+echo 1000 > /sys/class/backlight/gmux_backlight/brightness
