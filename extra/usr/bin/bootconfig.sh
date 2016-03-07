b684ee4a46d05a745457d0d32a6607d3c25cef1f
savoca <adeddo27@gmail.com>
2016-03-07 03:29:53 -0800

    Add bootconfig systemd service and startup script

---
+++ extra/usr/bin/bootconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Wait for a little while after logind to let everything settle
+sleep 3
+
+# Whatever you need here
+
+## Apple keyboard backlight control
+chmod 0666 /sys/class/leds/smc\:\:kbd_backlight/brightness
+
+## 2015 rMBP (and maybe others) gmux_backlight control
+setpci -v -H1 -s 00:01.00 BRIDGE_CONTROL=0
+chmod 0666 /sys/class/backlight/gmux_backlight/brightness
